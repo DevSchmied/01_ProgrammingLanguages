eae5f1636ab81b657d76751e6f876ceaa51648fb
DevSchmied <devschmied@yahoo.com>
2025-07-04 11:02:40 +0200

    Add files via upload

---
+++ C#/14_Interfaces/13_Interfaces/Program.cs
@@ -0,0 +1,58 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace _13_Interfaces
+{
+    // Simple interface with one method
+    interface IA
+    {
+        void foo();
+    }
+
+    // Another interface
+    interface IB
+    {
+        void bar();
+    }
+
+    // Third interface
+    interface IC
+    {
+        void qux();
+    }
+
+    // Interface inheritance
+    interface ID : IA, IB { }
+
+    // Class implements multiple interfaces
+    class MyBase : IC, ID
+    {
+        public void bar()
+        {
+            // ...
+        }
+
+        public void foo()
+        {
+            // ,,,
+        }
+
+        public void qux()
+        {
+            // ;;;
+        }
+    }
+
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            // Interface method call through instance
+            MyBase mb = new MyBase();
+            mb.foo();
+        }
+    }
+}
