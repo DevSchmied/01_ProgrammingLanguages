a0acef50b2bf2355e1e560db182907451856200b
DevSchmied <devschmied@yahoo.com>
2025-07-04 10:59:04 +0200

    Add files via upload

---
+++ C#/11_Exceptions/11_Exceptions/Program.cs
@@ -0,0 +1,37 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace _11_Exceptions
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            int a = 5;
+            int b = 0;
+            int result;
+
+            try
+            {
+                // Division by zero - triggers exception
+                result = a / b;
+            }
+            catch (Exception ex)
+            {
+                // Exception handling and message output
+                Console.WriteLine("Exception message: \"" + ex.Message + "\"");
+            }
+            finally
+            {
+                // Finally block always runs
+                Console.WriteLine("Always executed");
+            }
+
+            // Code after exception
+            Console.WriteLine("Program continued after exception.");
+        }
+    }
+}
