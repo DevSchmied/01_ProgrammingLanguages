58737b9f73059ea28b0dc08c33beb7aa496ef467
DevSchmied <devschmied@yahoo.com>
2025-07-04 09:04:19 +0200

    Create a_06_02_01_sv01.c

---
+++ C/18_Prototypen eigene_Header_Dateien/a_06_02_01_sv01/a_06_02_01_sv01.c
@@ -0,0 +1,27 @@
+#include<windows.h>
+#include<stdio.h>
+#include<string.h>
+#include "a_06_02_01_sv01.h"
+
+int main(void) {
+    system("chcp 1252");
+    system("cls");
+
+    int laenge;
+
+    printf("Geben Sie die L�nge eines Char-Arrays ein: ");
+    fflush(stdin);
+    scanf("%d", &laenge);
+
+    char arr[laenge];
+    printf("\nGeben Sie eine Zeile nicht l�nger als L�nge(-1) ein: ");
+    fflush(stdin);
+    scanf("%[^\n]", arr);
+
+
+    aufruf(laenge, arr);
+
+
+
+    return 0;
+}
