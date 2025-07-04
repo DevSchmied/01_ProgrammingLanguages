ce181f449405f4a67b5bd9fb7cd02d93a469d650
DevSchmied <devschmied@yahoo.com>
2025-07-04 10:53:59 +0200

    Add files via upload

---
+++ C#/06_Enum/06_Enum/Program.cs
@@ -0,0 +1,52 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace _06_Enum
+{
+    // Enum definition
+    enum Job
+    {
+        CheckStructure,
+        GetNewData,
+        RerenderPage
+    }
+
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            // Enum usage
+            Job job = Job.CheckStructure;
+
+            // Print enum name and value
+            Console.WriteLine(job);
+            Console.WriteLine((int)job);
+
+            // Enum in if-statement
+            if (job == Job.CheckStructure)
+            {
+                Console.WriteLine("CheckStructure");
+            }
+
+            // Enum in switch-statement
+            switch (job)
+            {
+                case Job.CheckStructure:
+                    Console.WriteLine("CheckStructure");
+                    break;
+                case Job.GetNewData:
+                    Console.WriteLine("GetNewData");
+                    break;
+                case Job.RerenderPage:
+                    Console.WriteLine("RerenderPage");
+                    break;
+                default:
+                    Console.WriteLine("Nothing");
+                    break;
+            }
+        }
+    }
+}
