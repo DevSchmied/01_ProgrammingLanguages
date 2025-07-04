16543937a70ecb7d4aa809efdf5aff4297833780
DevSchmied <devschmied@yahoo.com>
2025-07-04 10:59:50 +0200

    Add files via upload

---
+++ C#/12_AbstractClass/12_AbstractClass/Program.cs
@@ -0,0 +1,55 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace _12_AbstractClass
+{
+    // Abstract base class
+    abstract class Person
+    {
+        // Auto-properties for personal data
+        string firstname;
+        public string Firstname { get; set; }
+
+        string lastname;
+        public string Lastname { get; set; }
+
+        string street;
+        public string Street { get; set; }
+
+        string city;
+        public string City { get; set; }
+    }
+
+    // Derived class without additional members
+    class Customer : Person
+    {
+    }
+
+    // Derived class with method
+    class Employee : Person
+    {
+        decimal salary;
+
+        // Method of derived class
+        public void Work()
+        {
+            Console.WriteLine("I do my work!");
+        }
+    }
+
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            // Abstract class cannot be instantiated
+            // Person person = new Person();
+
+            // Create instances of derived classes
+            Customer customer = new Customer() { Firstname = "Tom", Lastname = "Johnson", City = "New York", Street = "Avenue Park" };
+            Employee employee = new Employee() { Firstname = "Sara", Lastname = "Taylor", City = "Chicago", Street = "Rosen Street" };
+        }
+    }
+}
