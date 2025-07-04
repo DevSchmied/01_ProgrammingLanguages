e9c202661047eafcb391240c589e3be34e5da31a
DevSchmied <devschmied@yahoo.com>
2025-07-04 11:08:20 +0200

    Add files via upload

---
+++ C#/19_TddFizzBuzzKata/18_TddFizzBuzzKata/Class1.cs
@@ -0,0 +1,28 @@
+﻿namespace _18_TddFizzBuzzKata
+{
+    // FizzBuzz core logic
+    public class FizzBuzzConverter
+    {
+        public string Convert(int value)
+        {
+            var result = string.Empty;
+
+            if (value % 3 == 0)
+            {
+                result = "Fizz";
+            }
+
+            if (value % 5 == 0)
+            {
+                result += "Buzz";
+            }
+
+            if (result != string.Empty)
+            {
+                return result;
+            }
+
+            return value.ToString();
+        }
+    }
+}
