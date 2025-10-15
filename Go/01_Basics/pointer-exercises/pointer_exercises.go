5f243f7d71fc3bab6a24d737a12dbb4f6297d0da
DevSchmied <devschmied@yahoo.com>
2025-10-15 15:36:14 +0200

    exercise(Go/01_Basics): implement Task 2 — string pointer example with value and address output

--- Go/01_Basics/pointer-exercises/pointer_exercises.go
+++ Go/01_Basics/pointer-exercises/pointer_exercises.go
@@ -15,6 +15,7 @@ func main() {
 			- Print the value of the pointer variable.
 			- Print the value stored at the address the pointer points to.
 	*/
+	fmt.Println("---------------------Task 1---------------------")
 
 	var num int
 	var numPtr *int
@@ -27,4 +28,30 @@ func main() {
 	fmt.Printf("Value of numPtr (address stored): %p\n", numPtr)
 	fmt.Printf("Value pointed to by numPtr: %d\n", *numPtr)
 
+	/*
+		Task 2 — String Pointer
+			- Declare a string variable.
+			- Declare a pointer variable to a string.
+			- Assign a text value to the string variable.
+			- Assign the address of the string variable to the pointer.
+			- Print the value of the string variable.
+			- Print the address of the string variable.
+			- Print the value of the pointer variable.
+			- Print the value stored at the address the pointer points to.
+	*/
+
+	fmt.Println()
+	fmt.Println("---------------------Task 2---------------------")
+
+	var str string
+	var strPtr *string
+
+	str = "Text"
+	strPtr = &str
+
+	fmt.Printf("Value of str: %s\n", str)
+	fmt.Printf("Address of str: %p\n", &str)
+
+	fmt.Printf("Value of strPtr (stored address): %p\n", strPtr)
+	fmt.Printf("Value pointed to by strPtr: %s\n", *strPtr)
 }
