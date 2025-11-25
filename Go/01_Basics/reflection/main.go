833dea4f0ffafd7dbc5762f28f1caee641a8c7e0
DevSchmied <devschmied@yahoo.com>
2025-11-25 12:02:27 +0100

    exercise(Go/01_Basics): add generalized task description

---
+++ Go/01_Basics/reflection/main.go
@@ -0,0 +1,21 @@
+package main
+
+/*
+TASK:
+
+Create a program that demonstrates the use of reflection by working with a simple data structure.
+
+Your program should:
+1. Define a structure representing a user with at least one field.
+2. Create an instance of this structure and assign a meaningful value to its field.
+3. Use reflection to inspect this instance at runtime and extract information about its data.
+4. Print both the retrieved value of the chosen field and the type information of the object.
+*/
+
+type User struct {
+	Name string
+}
+
+func main() {
+
+}
