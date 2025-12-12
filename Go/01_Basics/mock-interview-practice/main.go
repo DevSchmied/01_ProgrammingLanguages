8d5b5a849d4d6611375f531e0c4b8a3e60d319c9
DevSchmied <devschmied@yahoo.com>
2025-12-12 12:15:39 +0100

    feat(01_basics/mock-interview-practice): [Task 3] add concurrency-safe key-value cache with mutex

--- Go/01_Basics/mock-interview-practice/main.go
+++ Go/01_Basics/mock-interview-practice/main.go
@@ -1,6 +1,14 @@
 package main
 
-import "fmt"
+import (
+	"fmt"
+	"sync"
+)
+
+var (
+	mTask3 = make(map[string]string)
+	mu     sync.Mutex
+)
 
 func main() {
 	// Task 1. What will be printed to the console?
@@ -35,7 +43,7 @@ func main() {
 
 	// Task 2. Create a small concurrent program in Go that demonstrates a data race
 	// when multiple goroutines access and modify shared state without proper synchronization.
-
+	fmt.Println()
 	fmt.Println("Task 2")
 
 	m := map[int]*int{}
@@ -50,4 +58,39 @@ func main() {
 	}
 
 	fmt.Println(*m[0])
+
+	/*
+		Task 3.
+		Design a concurrency-safe key–value cache in Go that allows multiple goroutines to access shared data.
+		The cache should return an existing value for a given key if it is already present, or create and store a new value otherwise.
+		Ensure that all operations are safe under concurrent access.
+	*/
+	fmt.Println()
+	fmt.Println("Task 3")
+
+	fmt.Println(GetOrCreate("hello", "world"))
+	fmt.Println(GetOrCreate("hello", "world"))
+	fmt.Println(Get("hello"))
+
+}
+
+// Task 3.
+func GetOrCreate(key, value string) string {
+	mu.Lock()
+	defer mu.Unlock()
+	if v, ok := mTask3[key]; ok {
+		return v
+	}
+
+	mTask3[key] = value
+	return value
+}
+
+func Get(key string) string {
+	mu.Lock()
+	defer mu.Unlock()
+	if v, ok := mTask3[key]; ok {
+		return v
+	}
+	return ""
 }
