9925eff147f0876e9f97eba8744843b30e27e082
DevSchmied <devschmied@yahoo.com>
2025-12-12 10:05:01 +0100

    feat(01_basics/mock-interview-practice): add string iteration interview task with UTF-8 runes example

---
+++ Go/01_Basics/mock-interview-practice/main.go
@@ -0,0 +1,21 @@
+package main
+
+import "fmt"
+
+func main() {
+	// Task: What will be printed to the console?
+	// Explain how string iteration works in Go when using range,
+	// especially with UTF-8 encoded characters (runes).
+
+	str1 := "hello 🙂💪"
+
+	for i, char := range str1 {
+		fmt.Println(i, ": ", char)
+	}
+
+	fmt.Println()
+	for i, char := range str1 {
+		fmt.Printf("%d: %c\n", i, char)
+	}
+
+}
