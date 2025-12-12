609d48535a322fccd650171d1bc364a315975782
DevSchmied <devschmied@yahoo.com>
2025-12-12 10:08:48 +0100

    feat(01_basics/mock-interview-practice): add Cyrillic string example to compare UTF-8 rune iteration

--- Go/01_Basics/mock-interview-practice/main.go
+++ Go/01_Basics/mock-interview-practice/main.go
@@ -8,6 +8,7 @@ func main() {
 	// especially with UTF-8 encoded characters (runes).
 
 	str1 := "hello 🙂💪"
+	str2 := "привет 🙂💪"
 
 	for i, char := range str1 {
 		fmt.Println(i, ": ", char)
@@ -15,6 +16,18 @@ func main() {
 
 	fmt.Println()
 	for i, char := range str1 {
+		fmt.Printf("%d: %c\n", i, char)
+	}
+
+	fmt.Println()
+	fmt.Println()
+
+	for i, char := range str2 {
+		fmt.Println(i, ": ", char)
+	}
+
+	fmt.Println()
+	for i, char := range str2 {
 		fmt.Printf("%d: %c\n", i, char)
 	}
 
