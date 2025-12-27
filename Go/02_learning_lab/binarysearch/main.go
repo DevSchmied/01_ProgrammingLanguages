73cc6e143be2f8d8302b757febab9aeb0c7f6f74
DevSchmied <devschmied@yahoo.com>
2025-12-27 18:54:25 +0100

    exercise(Go/02_learning_lab): [binary-search] implement binary search algorithm

--- Go/02_learning_lab/binarysearch/main.go
+++ Go/02_learning_lab/binarysearch/main.go
@@ -21,6 +21,20 @@ Explanation: 2 does not exist in nums so return -1
 */
 
 func search(nums []int, target int) int {
+	left, right := 0, len(nums)-1
+
+	for left <= right {
+		mid := left + (right-left)/2
+
+		if nums[mid] == target {
+			return mid
+		}
+		if nums[mid] > target {
+			right = mid - 1
+		} else {
+			left = mid + 1
+		}
+	}
 	return -1
 }
 
