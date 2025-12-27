35cb1c42b76fe6b7a4a0f24c8f5eb0be368cb904
DevSchmied <devschmied@yahoo.com>
2025-12-27 18:31:40 +0100

    exercise(Go/02_learning_lab): [binary-search] add binary search function signature

--- Go/02_learning_lab/binarysearch/main.go
+++ Go/02_learning_lab/binarysearch/main.go
@@ -19,6 +19,11 @@ Input: nums = [-1,0,3,5,9,12], target = 2
 Output: -1
 Explanation: 2 does not exist in nums so return -1
 */
+
+func search(nums []int, target int) int {
+	return -1
+}
+
 func main() {
 
 }
