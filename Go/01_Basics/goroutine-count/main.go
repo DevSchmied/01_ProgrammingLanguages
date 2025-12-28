82b8933967514faf8d656112f0c433c6e8a91ee9
DevSchmied <devschmied@yahoo.com>
2025-12-28 13:19:28 +0100

    exercise(Go/01_basics): [count goroutines] add task description

---
+++ Go/01_Basics/goroutine-count/main.go
@@ -0,0 +1,6 @@
+package main
+
+/*
+Task:
+Create a program that starts multiple concurrent tasks, keeps them running for a short period of time, and reports how many concurrent executions are active at a given moment.
+*/
