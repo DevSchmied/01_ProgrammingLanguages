368132eb8de9a5488aeeb0e652f3dd9a8c5add46
DevSchmied <devschmied@yahoo.com>
2025-12-28 13:44:27 +0100

    exercise(Go/01_basics): [count goroutines] demonstrate counting active concurrent executions with runtime.NumGoroutine()

--- Go/01_Basics/goroutine-count/main.go
+++ Go/01_Basics/goroutine-count/main.go
@@ -4,3 +4,21 @@ package main
 Task:
 Create a program that starts multiple concurrent tasks, keeps them running for a short period of time, and reports how many concurrent executions are active at a given moment.
 */
+import (
+	"fmt"
+	"runtime"
+	"time"
+)
+
+func main() {
+
+	for i := 0; i < 100; i++ {
+		go func() {
+			time.Sleep(2 * time.Second)
+		}()
+	}
+
+	time.Sleep(1 * time.Second)
+	fmt.Println("Number of goroutines:", runtime.NumGoroutine())
+
+}
