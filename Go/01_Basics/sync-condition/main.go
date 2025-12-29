637994ba9061402d7fc670b8ac4998c5d52d48a5
DevSchmied <devschmied@yahoo.com>
2025-12-29 09:09:29 +0100

    exercise(Go/01_basics): [sync.Cond] demonstrate condition variable synchronization

--- Go/01_Basics/sync-condition/main.go
+++ Go/01_Basics/sync-condition/main.go
@@ -5,3 +5,38 @@ Task:
 Implement a program where one task waits for a specific condition to become true before continuing its execution.
 Another task should update the shared state and notify the waiting task when the condition is satisfied.
 */
+
+import (
+	"fmt"
+	"sync"
+	"time"
+)
+
+func main() {
+	var wg sync.WaitGroup
+	mu := &sync.Mutex{}
+	cond := sync.NewCond(mu)
+
+	ready := false
+
+	wg.Add(1)
+	go func() {
+		defer wg.Done()
+		mu.Lock()
+		for !ready {
+			fmt.Println("Waiting...")
+			cond.Wait()
+		}
+		fmt.Println("Condition met")
+		mu.Unlock()
+	}()
+
+	time.Sleep(2 * time.Second)
+
+	mu.Lock()
+	ready = true
+	cond.Signal()
+	mu.Unlock()
+
+	wg.Wait()
+}
