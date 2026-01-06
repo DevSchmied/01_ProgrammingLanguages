883cba0eecebc2631348cf415fd17ff98d00e8ed
DevSchmied <devschmied@yahoo.com>
2026-01-06 13:05:04 +0100

    exercise(Go/02_learning_lab) [shuffle-string] implement brute-force solution with nested loops and tests

--- Go/02_learning_lab/shuffle-string/main.go
+++ Go/02_learning_lab/shuffle-string/main.go
@@ -35,7 +35,29 @@ indices = [0,1,2]
 Output:
 "abc"
 */
+import "fmt"
+
+func restoreStringBySearch(s string, indices []int) string {
+	if len(s) != len(indices) {
+		return ""
+	}
+
+	resRunes := make([]rune, 0, len(s))
+
+	for i := 0; i < len(s); i++ {
+		for j := 0; j < len(s); j++ {
+			if indices[j] == i {
+				resRunes = append(resRunes, rune(s[j]))
+				break
+			}
+		}
+	}
+
+	return string(resRunes)
+}
 
 func main() {
-
+	fmt.Println("=======================1. Solution=======================")
+	fmt.Println(restoreStringBySearch("codeleet", []int{4, 5, 6, 7, 0, 2, 1, 3}))
+	fmt.Println(restoreStringBySearch("abc", []int{0, 1, 2}))
 }
