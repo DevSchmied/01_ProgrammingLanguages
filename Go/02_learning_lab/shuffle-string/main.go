d7ff23fe553a5ab5208abac21d1d9cb58f9cf2b5
DevSchmied <devschmied@yahoo.com>
2026-01-07 12:41:37 +0100

    docs(Go/02_learning_lab) [shuffle-string] add missing task description

--- Go/02_learning_lab/shuffle-string/main.go
+++ Go/02_learning_lab/shuffle-string/main.go
@@ -1,5 +1,39 @@
 package main
 
+/* Leetcodetask: 1528. Shuffle String
+
+You are given a string s and an integer array indices of the same length.
+
+The string s will be shuffled such that the character at the i-th position
+moves to indices[i] in the shuffled string.
+
+Return the shuffled string.
+
+Example 1:
+Input:
+    s = "codeleet"
+    indices = [4,5,6,7,0,2,1,3]
+Output:
+    "leetcode"
+Explanation:
+    "codeleet" becomes "leetcode" after shuffling.
+
+Example 2:
+Input:
+    s = "abc"
+    indices = [0,1,2]
+Output:
+    "abc"
+Explanation:
+    Each character remains in its original position.
+
+Constraints:
+    - s.length == indices.length == n
+    - 1 <= n <= 100
+    - s consists only of lowercase English letters
+    - 0 <= indices[i] < n
+    - All values in indices are unique
+*/
 import "fmt"
 
 func restoreStringBySearch(s string, indices []int) string {
