4277b4af7793e1b92e6a34eaca192e069462850b
DevSchmied <devschmied@yahoo.com>
2026-01-14 14:25:59 +0100

    exercise(Go/02_learning_lab): [linked-list-cycle] add LeetCode #141 solution

--- Go/02_learning_lab/linked-list-cycle/main.go
+++ Go/02_learning_lab/linked-list-cycle/main.go
@@ -36,3 +36,79 @@ Constraints:
 - -10^5 <= Node.val <= 10^5
 - pos is -1 or a valid index in the linked list.
 */
+
+import "fmt"
+
+type ListNode struct {
+	Val  int
+	Next *ListNode
+}
+
+func hasCycle(head *ListNode) bool {
+	if head == nil {
+		return false
+	}
+
+	visited := make(map[*ListNode]struct{})
+	cur := head
+
+	for cur != nil {
+		if _, ok := visited[cur]; ok {
+			return true
+		}
+		visited[cur] = struct{}{}
+		cur = cur.Next
+	}
+
+	return false
+}
+
+func main() {
+	fmt.Println("=================1. Example=================")
+
+	var ln1, ln2, ln3, ln4 ListNode
+
+	ln4 = ListNode{
+		Val:  -4,
+		Next: &ln2,
+	}
+
+	ln3 = ListNode{
+		Val:  0,
+		Next: &ln4,
+	}
+
+	ln2 = ListNode{
+		Val:  2,
+		Next: &ln3,
+	}
+
+	ln1 = ListNode{
+		Val:  3,
+		Next: &ln2,
+	}
+
+	fmt.Println(hasCycle(&ln1))
+
+	fmt.Println("=================2. Example=================")
+
+	var ln21, ln22 ListNode
+
+	ln21.Val = 1
+	ln21.Next = &ln22
+
+	ln22.Val = 2
+	ln22.Next = &ln21
+
+	fmt.Println(hasCycle(&ln21))
+
+	fmt.Println("=================3. Example=================")
+
+	ln31 := ListNode{
+		Val:  1,
+		Next: nil,
+	}
+
+	fmt.Println(hasCycle(&ln31))
+
+}
