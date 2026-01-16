a30da751b696f0cbd8ebbc18ca9058ae38b288e7
DevSchmied <devschmied@yahoo.com>
2026-01-16 12:07:09 +0100

    exercise(Go/02_learning_lab): [majority element] add LeetCode #169 solution

--- Go/02_learning_lab/majority-element/main.go
+++ Go/02_learning_lab/majority-element/main.go
@@ -1,5 +1,7 @@
 package main
 
+import "fmt"
+
 /*
 LeetCode 169 — Majority Element
 
@@ -28,3 +30,34 @@ Constraints:
 Source:
 This is a problem from the LeetCode platform.
 */
+
+func majorityElement(nums []int) int {
+
+	valuesMap := make(map[int]int)
+
+	for _, v := range nums {
+		valuesMap[v]++
+	}
+
+	counter := 0
+	result := nums[0]
+	for k, v := range valuesMap {
+		if counter < v {
+			counter = v
+			result = k
+		}
+	}
+	if counter > len(nums)/2 {
+		return result
+	}
+	return 0
+}
+
+func main() {
+
+	fmt.Printf("Example 1 | Expected: %d | Got: %d\n", 3, majorityElement([]int{3, 2, 3}))
+
+	fmt.Println()
+
+	fmt.Printf("Example 2 | Expected: %d | Got: %d\n", 2, majorityElement([]int{2, 2, 1, 1, 1, 2, 2}))
+}
