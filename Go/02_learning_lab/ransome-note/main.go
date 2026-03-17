79a68c7d87b33c73fd6d0c32d9d4bec16e6ea5e8
DevSchmied <devschmied@yahoo.com>
2026-03-17 19:00:03 +0300

    exercise(Go/02_learning_lab): [ransome-note] add LeetCode #383 problem solution

--- Go/02_learning_lab/ransome-note/main.go
+++ Go/02_learning_lab/ransome-note/main.go
@@ -1,5 +1,7 @@
 package main
 
+import "fmt"
+
 /*
 LeetCode 383: Ransom Note
 
@@ -23,3 +25,25 @@ Constraints:
 1 <= ransomNote.length, magazine.length <= 10^5
 ransomNote and magazine consist of lowercase English letters only
 */
+
+func canConstruct(ransomNote string, magazine string) bool {
+
+	mapMagazine := make(map[rune]int)
+
+	for _, v := range magazine {
+		mapMagazine[v]++
+		fmt.Println("v:", string(v))
+	}
+	fmt.Println(mapMagazine)
+
+	for _, v := range ransomNote {
+		fmt.Println("v2:", mapMagazine[v])
+		_, exists := mapMagazine[v]
+		if exists && mapMagazine[v] != 0 {
+			mapMagazine[v]--
+		} else {
+			return false
+		}
+	}
+	return true
+}
