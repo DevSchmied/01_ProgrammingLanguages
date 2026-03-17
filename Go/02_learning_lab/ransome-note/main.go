d797044f2827d555a7a5e14dae5b5ebd8b8b6130
DevSchmied <devschmied@yahoo.com>
2026-03-17 10:21:44 +0300

    exercise(Go/02_learning_lab): [ransome-note] add LeetCode #383 problem description

---
+++ Go/02_learning_lab/ransome-note/main.go
@@ -0,0 +1,25 @@
+package main
+
+/*
+LeetCode 383: Ransom Note
+
+Note: This problem is from LeetCode.
+
+Problem Statement:
+Given two strings ransomNote and magazine, return true if ransomNote can be constructed by using the letters from magazine and false otherwise.
+Each letter in magazine can only be used once in ransomNote.
+
+Examples:
+Input: ransomNote = "a", magazine = "b"
+Output: false
+
+Input: ransomNote = "aa", magazine = "ab"
+Output: false
+
+Input: ransomNote = "aa", magazine = "aab"
+Output: true
+
+Constraints:
+1 <= ransomNote.length, magazine.length <= 10^5
+ransomNote and magazine consist of lowercase English letters only
+*/
