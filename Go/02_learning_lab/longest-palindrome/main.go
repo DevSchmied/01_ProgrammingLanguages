6dee37f2215c88969000df73f340be6cbe41e3bf
DevSchmied <devschmied@yahoo.com>
2026-03-18 15:36:51 +0300

    exercise(Go/02_learning_lab): [longest-palindrome] add LeetCode #409 problem description

---
+++ Go/02_learning_lab/longest-palindrome/main.go
@@ -0,0 +1,31 @@
+package main
+
+/*
+LeetCode 409: Longest Palindrome
+
+Problem Statement:
+
+Given a string s which consists of lowercase or uppercase letters, return the length of the longest palindrome that can be built with those letters.
+
+Letters are case sensitive, for example, "Aa" is not considered a palindrome.
+
+Examples:
+Input: s = "abccccdd"
+Output: 7
+Explanation: One longest palindrome that can be built is "dccaccd", whose length is 7.
+
+Input: s = "a"
+Output: 1
+Explanation: The longest palindrome that can be built is "a", whose length is 1.
+
+Constraints:
+1 <= s.length <= 2000
+s consists of lowercase and/or uppercase English letters only.
+
+Note: This problem is from LeetCode and requires finding the maximum possible palindrome length using the given characters.
+*/
+
+func longestPalindrome(s string) int {
+
+	return 1
+}
