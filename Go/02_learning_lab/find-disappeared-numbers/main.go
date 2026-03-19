0d197b1d2194d64efd830bd9c619c3b5ff31a430
DevSchmied <devschmied@yahoo.com>
2026-03-19 11:17:16 +0300

    exercise(Go/02_learning_lab): [find-disappeared-numbers] add LeetCode #448 problem description

---
+++ Go/02_learning_lab/find-disappeared-numbers/main.go
@@ -0,0 +1,24 @@
+package main
+
+/*
+LeetCode 448: Find All Numbers Disappeared in an Array
+Quelle: LeetCode (https://leetcode.com)
+
+Problem Statement:
+
+Given an array nums of n integers where nums[i] is in the range [1, n], return an array of all the integers in the range [1, n] that do not appear in nums.
+
+Examples:
+Input: nums = [4,3,2,7,8,2,3,1]
+Output: [5,6]
+Explanation: The numbers 1 through 8 should appear, but 5 and 6 are missing.
+
+Input: nums = [1,1]
+Output: [2]
+Explanation: The numbers 1 through 2 should appear, but 2 is missing.
+
+Constraints:
+n == nums.length
+1 <= n <= 10^5
+1 <= nums[i] <= n
+*/
