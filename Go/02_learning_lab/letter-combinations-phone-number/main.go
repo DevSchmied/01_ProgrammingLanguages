3de3154e49fa62e688137fbf77bf8e6990ac0be3
DevSchmied <devschmied@yahoo.com>
2026-03-25 17:58:45 +0300

    exercise(Go/02_learning_lab): [letter-combinations-phone-number] add LeetCode #17 problem description

---
+++ Go/02_learning_lab/letter-combinations-phone-number/main.go
@@ -0,0 +1,44 @@
+package main
+
+/*
+LeetCode 17: Letter Combinations of a Phone Number
+Quelle: LeetCode (https://leetcode.com/problems/letter-combinations-of-a-phone-number)
+
+Problem Statement:
+
+Given a string containing digits from 2-9 inclusive, return all possible letter combinations that the number could represent. Return the answer in any order.
+
+A mapping of digits to letters (just like on the telephone buttons) is given below. Note that 1 does not map to any letters.
+
+Digit to letter mapping:
+2 -> "abc"
+3 -> "def"
+4 -> "ghi"
+5 -> "jkl"
+6 -> "mno"
+7 -> "pqrs"
+8 -> "tuv"
+9 -> "wxyz"
+
+Examples:
+Input: digits = "23"
+Output: ["ad","ae","af","bd","be","bf","cd","ce","cf"]
+Explanation: Each digit maps to a set of letters. All combinations are formed by taking one letter from the first digit's mapping and one letter from the second digit's mapping.
+
+Input: digits = "2"
+Output: ["a","b","c"]
+Explanation: Only one digit, so return all letters that digit maps to.
+
+Input: digits = ""
+Output: []
+Explanation: Empty string returns an empty list.
+
+Constraints:
+1 <= digits.length <= 4
+digits[i] is a digit in the range ['2', '9']
+*/
+
+func letterCombinations(digits string) []string {
+
+	return []string{}
+}
