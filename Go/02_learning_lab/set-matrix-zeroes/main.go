b9ad4e6fc19a4caaac2250bb68c740d86af57490
DevSchmied <devschmied@yahoo.com>
2026-03-31 15:19:02 +0300

    exercise(Go/02_learning_lab): [set-matrix-zeroes] add LeetCode #73 problem description

---
+++ Go/02_learning_lab/set-matrix-zeroes/main.go
@@ -0,0 +1,31 @@
+package main
+
+/*
+LeetCode 73: Set Matrix Zeroes
+Quelle: LeetCode (https://leetcode.com)
+
+Problem Statement:
+
+Given an m x n integer matrix matrix, if an element is 0, set its entire row and column to 0's.
+
+You must do it in place.
+
+Examples:
+Input: matrix = [[1,1,1],[1,0,1],[1,1,1]]
+Output: [[1,0,1],[0,0,0],[1,0,1]]
+Explanation: The element at position (1,1) is 0, so row 1 and column 1 are set to 0.
+
+Input: matrix = [[0,1,2,0],[3,4,5,2],[1,3,1,5]]
+Output: [[0,0,0,0],[0,4,5,0],[0,3,1,0]]
+Explanation: The zeros at positions (0,0) and (0,3) cause row 0, column 0, and column 3 to be set to 0. Row 1 and row 2 have zeros in affected columns.
+
+Constraints:
+m == matrix.length
+n == matrix[0].length
+1 <= m, n <= 200
+-2^31 <= matrix[i][j] <= 2^31 - 1
+*/
+
+func setZeroes(matrix [][]int) {
+
+}
