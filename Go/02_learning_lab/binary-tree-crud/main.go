d88bd8eb42713168f6fa242dab3806a0161be35c
DevSchmied <devschmied@yahoo.com>
2026-04-03 15:43:51 +0300

    exercise(Go/02_learning_lab): [binary-tree-crud] add deleteNode function

--- Go/02_learning_lab/binary-tree-crud/main.go
+++ Go/02_learning_lab/binary-tree-crud/main.go
@@ -27,6 +27,49 @@ func insertNode(rootNode *Node, node *Node) *Node {
 	return rootNode
 }
 
+func deleteNode(rootNode *Node, node *Node) *Node {
+	// Base case: Empty position found
+	if rootNode == nil {
+		return nil
+	}
+
+	// Case 1: Value is GREATER. Delete from RIGHT subtree
+	if node.Val > rootNode.Val {
+		rootNode.Right = deleteNode(rootNode.Right, node)
+		return rootNode
+	}
+
+	// Case 2: Value is SMALLER. Delete from LEFT subtree
+	if node.Val < rootNode.Val {
+		rootNode.Left = deleteNode(rootNode.Left, node)
+		return rootNode
+	}
+
+	// Case 3: Value FOUND. Delete this node
+	// Subcase 1: No children
+	if rootNode.Left == nil && rootNode.Right == nil {
+		return nil
+	}
+
+	// Subcase 2: One child
+	if rootNode.Left == nil {
+		return rootNode.Right
+	}
+	if rootNode.Right == nil {
+		return rootNode.Left
+	}
+
+	// Subcase 3: Two children - replace with inorder successor
+	successor := rootNode.Right
+	for successor.Left != nil {
+		successor = successor.Left
+	}
+	rootNode.Val = successor.Val
+	rootNode.Right = deleteNode(rootNode.Right, &Node{Val: successor.Val})
+
+	return rootNode
+}
+
 func main() {
 	var root *Node
 
@@ -38,11 +81,14 @@ func main() {
 	root = insertNode(root, &Node{Val: 6})
 	root = insertNode(root, &Node{Val: 8})
 
-	fmt.Println("Root:", root.Val)                    // 5
-	fmt.Println("Left:", root.Left.Val)               // 3
-	fmt.Println("Right:", root.Right.Val)             // 7
-	fmt.Println("Left-Left:", root.Left.Left.Val)     // 2
-	fmt.Println("Left-Right:", root.Left.Right.Val)   // 4
-	fmt.Println("Right-Left:", root.Right.Left.Val)   // 6
-	fmt.Println("Right-Right:", root.Right.Right.Val) // 8
+	fmt.Println("Initial tree after inserts:")
+	fmt.Println("  Root:", root.Val)                    // 5
+	fmt.Println("  Left:", root.Left.Val)               // 3
+	fmt.Println("  Right:", root.Right.Val)             // 7
+	fmt.Println("  Left-Left:", root.Left.Left.Val)     // 2
+	fmt.Println("  Left-Right:", root.Left.Right.Val)   // 4
+	fmt.Println("  Right-Left:", root.Right.Left.Val)   // 6
+	fmt.Println("  Right-Right:", root.Right.Right.Val) // 8
+	fmt.Println()
+
 }
